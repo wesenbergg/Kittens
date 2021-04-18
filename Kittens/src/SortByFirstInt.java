0a3e47ddef0782d5f36b693d9dfe7988adad1591
wesenbergg <b.b.j.1998@gmail.com>
2021-04-19 00:32:58 +0300

    Comment
    - added comment to comparator class

--- Kittens/src/SortByFirstInt.java
+++ Kittens/src/SortByFirstInt.java
@@ -1,10 +1,14 @@
 import java.util.Comparator;
 
+/**
+ * Compares first two integers of the list.
+ * @author Boriss
+ *
+ */
 public class SortByFirstInt implements Comparator<int[]> {
 	@Override
 	public int compare(int[] list1, int[] list2) {
 		int diff = list1[0] - list2[0];
-		
 		return (diff == 0) ? list1[1] - list2[1]: diff;
 	}
 }
