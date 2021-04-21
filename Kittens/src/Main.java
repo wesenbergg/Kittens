681afb78438606ffb3523f5fb759b4d7e7d2b1f3
wesenbergg <b.b.j.1998@gmail.com>
2021-04-21 13:47:17 +0300

    tehtävänanto

--- Kittens/src/Main.java
+++ Kittens/src/Main.java
@@ -5,9 +5,16 @@ public class Main {
 
 	/**
 	 *	Tehtävänanto:
-	 *  n and k (1≤k<n≤100000), the number of potential visiting kittens and the number of beds
+	 *  Syöte: n and k (1≤k<n≤100000), the number of potential visiting kittens and the number of beds
 	 *  Then follow n lines, each containing two integers xi and yi, meaning that kitten i wants to arrive at time xi and leave at time yi.
 	 *  0≤xi<yi≤1000000000
+	 *  
+	 *  Halutaan saada selville mikä on maksimi määrä kissoja joille voidaan tarjota sänkyjä. Yksi sänky voi ottaa vastaan yhden kissan,
+	 *  ja yksi sänky on varattuna kissalle koko vierailun ajaksi. Kissan vierailun jälkeen voidaan sänkyyn ottaa uusi kissa.
+	 *  Esimerkiksi jos kissa1 arvot ovat 1 3 ja kissa2 arvot ovat 3 5, niin voivat nämä kissat jakaa saman sängyn.
+	 *  Esimerkiksi jos kissa1 arvot ovat 1 3 ja kissa2 arvot ovat 2 4, niin nämä kissat eivät voi jakaa samaa sänkyä.
+	 *  
+	 *  Ohjelma siis tulostaa vain maksimi määrän kissoja mitä voi ottaa vastaan.
 	 */
 	
 	/**
